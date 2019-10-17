1091954f8c459b6694a44cc3f8239e2d41778c66
bhadurianirban <bhaduri.anirban@gmail.com>
2019-10-17 15:21:56 +0530

    Muon bhai

--- Mergeeta.py
+++ Mergeeta.py
@@ -8,7 +8,7 @@ class Mergeeeta:
         print("gheu")
 
     def read_and_merge_eta(self):
-        muondata = pd.read_csv('/home/bhaduri/MEGA/dimuon/data/ephimassdi7.txt', delimiter=';', header=None)
+        muondata = pd.read_csv('/home/dgrfi/MEGA/dimuon/data/ephimassdi8.txt', delimiter=';', header=None)
         # print (muondata)
 
         # muondata.to_csv('/home/dgrfi/MEGA/dimuon/data/gheu.txt', header=None, columns=[0], index=None)
@@ -20,12 +20,16 @@ class Mergeeeta:
             rowcount = rowcount + 1
             muoneta.loc[rowcount] = row[2]
             rowcount = rowcount + 1
-        muoneta.to_csv('/home/bhaduri/MEGA/dimuon/data/gheu.txt',index=None)
+        muoneta.to_csv('/home/dgrfi/MEGA/dimuon/data/gheu8.txt',index=None)
 
     def plot_eta(self):
-        muoneta = pd.read_csv('/home/bhaduri/MEGA/dimuon/data/gheu.txt')
-        muoneta.reset_index().plot(kind='scatter', x='index', y='eta')
+        muoneta = pd.read_csv('/home/dgrfi/MEGA/dimuon/data/gheu8.txt')
+        print(muoneta)
+        # muonetawithindex = muoneta.reset_index()
+        muoneta.hist(bins=20)
         plt.show()
+        # muoneta.reset_index().plot(kind='scatter', x='index', y='eta')
+        # plt.show()
         # muoneta.plot(x=muoneta.index, y='eta', kind='scatter')
         # plt.xticks(muoneta['eta'], muoneta.index.values)  # location, labels
         # plt.plot(muoneta['eta'], type='scatter')
--- Muonmain.py
+++ Muonmain.py
@@ -1,7 +1,7 @@
 from  Mergeeta import Mergeeeta
 
 mg = Mergeeeta()
-# mg.read_and_merge_eta()
+mg.read_and_merge_eta()
 mg.plot_eta()
 
 
