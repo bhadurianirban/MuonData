567042715b07b1999b624b959cbabe912267c94b
bhadurianirban <bhaduri.anirban@gmail.com>
2019-11-20 15:24:45 +0530

    Muon bhai

--- Muonml.py
+++ Muonml.py
@@ -6,7 +6,7 @@ class Muonml :
         print("ML")
 
     def read_muon_data(self):
-        column_names = ['muon_eta_1', 'muon_eta_2', 'muon_phi_1','muon_phi_2', 'event_number', 'run_number', 'luminocity']
-        muondata8 = pd.read_csv('/home/dgrfi/MEGA/dimuon/data/ephimassdi8.txt', delimiter=';', header=None, names=column_names, index_col='False')
+        column_names = ['muon_eta_1', 'muon_eta_2', 'muon_phi_1','muon_phi_2','gheu', 'event_number', 'run_number', 'luminocity']
+        muondata8 = pd.read_csv('/home/dgrfi/MEGA/dimuon/data/ephimassdi8.txt', delimiter=';', header=None, names=column_names)
 
         print(muondata8.head())
