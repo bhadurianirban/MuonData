5c6be6779f74044c3fb5a7e3a0e74fed6cac46b1
bhaduri <bhaduri.anriban@gmail.com>
2019-12-11 21:54:26 +0530

    ghar

--- SuperSymmetry.py
+++ SuperSymmetry.py
@@ -1,5 +1,7 @@
-import  pandas as pd
-
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
 
 class SuperSymmetry:
     scale_up_value = 10000
@@ -13,8 +15,19 @@ class SuperSymmetry:
         momentum_df = rawdf[['Px', 'Py', 'Pz']]
         min_px_py = momentum_df.min()
 
-        momentum_Px_Py_Pz = pd.DataFrame(columns=['Px', 'Py', 'Pz'])
-        momentum_Px_Py_Pz['Px'] = (momentum_df.loc[:, 'Px'] - min_px_py['Px']) * self.scale_up_value
-        momentum_Px_Py_Pz['Py'] = (momentum_df.loc[:, 'Py'] - min_px_py['Py']) * self.scale_up_value
-        momentum_Px_Py_Pz['Pz'] = (momentum_df.loc[:, 'Pz'] - min_px_py['Pz']) * self.scale_up_value
-        print(momentum_Px_Py_Pz.head())
+        # momentum_Px_Py_Pz = pd.DataFrame(columns=['Px', 'Py', 'Pz'])
+
+        Px_value = momentum_df['Px']
+        Py_value = momentum_df['Py']
+        # plt.scatter(Px_value, Py_value)
+        # plt.show()
+
+        momentum_df = momentum_df[(np.abs(stats.zscore(momentum_df)) < 3).all(axis=1)]
+        Px_value = momentum_df['Px']
+        Py_value = momentum_df['Py']
+        plt.scatter(Px_value, Py_value)
+        plt.show()
+        # momentum_Px_Py_Pz['Px'] = (momentum_df.loc[:, 'Px'] - min_px_py['Px']) * self.scale_up_value
+        # momentum_Px_Py_Pz['Py'] = (momentum_df.loc[:, 'Py'] - min_px_py['Py']) * self.scale_up_value
+        # momentum_Px_Py_Pz['Pz'] = (momentum_df.loc[:, 'Pz'] - min_px_py['Pz']) * self.scale_up_value
+        # print(momentum_Px_Py_Pz.head())
