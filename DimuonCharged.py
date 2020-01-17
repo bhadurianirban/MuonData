9ee497e6ace96e4095952953b6856c495e95f2a7
bhadurianirban <bhaduri.anirban@gmail.com>
2020-01-17 15:20:59 +0530

    DGRF

--- DimuonCharged.py
+++ DimuonCharged.py
@@ -7,5 +7,11 @@ class DimuonCharged:
     def extract_dimuon_charged(self, input_file_path, tev):
         outputfile_prefix = 'dm'+tev
         orig_df = pd.read_csv(input_file_path, delimiter=';')
-        orig_df_pos = orig_df[orig_df['Charge'] == 1]
-        print(orig_df_pos.head())
+        df_neg = orig_df[orig_df['Charge'] == -1]
+        df_pos = orig_df[orig_df['Charge'] == 1]
+        df_pos_eta = df_pos['Eta']
+        df_neg_eta = df_neg['Eta']
+        df_pos_phi = df_pos['Phi']
+        df_neg_phi = df_neg['Phi']
+
+
